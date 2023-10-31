cdc581e0c17742e92bbfd0931ecf2c0a5b21b623
Willie Jones <williejones432@yahoo.com>
2023-10-31 13:38:08 -0500

    Added API url and key to Js

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,11 @@
-const weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+// Makes sure paage renders fully
+$(document).ready(function() {
+    const apiKey = 'd9593fee53a8685d50e5bbf150c861b9';
+
+    const getWeatherData = city => {
+        const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}`;
+        const forecastUrl = `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}`; 
+    }
+
+    
+});
