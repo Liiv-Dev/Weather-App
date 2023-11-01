d28fdcf46a670d662e0ea51ece44fcf1e0ce53d3
Willie Jones <williejones432@yahoo.com>
2023-11-01 13:27:01 -0500

    container layout adjusted and js searchbar function started

--- Assets/script.js
+++ Assets/script.js
@@ -6,14 +6,32 @@ $(document).ready(function() {
     // Array of cities for list buttons and to display forecast
     const cities = ['Albuquerque', 'Boston', 'Chicago', 'Columbus', 'Dallas', 'Fort Worth', 'Houston', 'Jacksonville', 'Las Vegas', 'Los Angeles', 'Madison', 'New York', 'Oklahoma City', 'Philadelphia', 'Phoenix', 'Portland', 'San Diego', 'San Francisco']
 
-     //Creates Search element for list container
-     $('.list-container').append(`<input id="userSearch" class="input" type="text" placeholder="Search">`);
+    /*
+     // variables get the input element and characters used in button cities
+     var searchBar = $('#userSearch');
+     var cityButtons = $('#city');
+
+     // Function hides buttons if search input doesnt match button characters 
+     searchBar.on('input', function(){
+        var searchChar = $(this).input().toLowerCase()
+
+        cityButtons.each(function() {
+            var buttonChar = $(this).text().toLowerCase();
+            if (buttonChar.includes(searchChar)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }   
+        })
+     })
+     */
+     
 
      // Loops through Cities Array
      for (var i = 0; i < cities.length; i++) {
          
          //Creates new option element
-         var citiesItem = $(`<button class='has-background-white has-text-black is-size-6'>`).html(cities[i]);
+         var citiesItem = $(`<button id='city' class='has-background-white has-text-black is-size-6'>`).html(cities[i]);
          
          // Append <option> to select element with city names
           $('.list-container').append(citiesItem)
@@ -21,7 +39,7 @@ $(document).ready(function() {
 
     //API URLs for current weather and 5 day forecast
     const getWeatherData = city => {
-        const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}`;
+        const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
         const forecastUrl = `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}`; 
     }
 
